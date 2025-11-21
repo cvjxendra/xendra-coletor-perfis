2bec2223abdbd80994cddb1ddb700a18d1de12c3
Junior <cvjcvj@gmail.com>
2025-11-21 17:33:07 -0300

    gpush

---
+++ gpush.bat
@@ -0,0 +1,17 @@
+@echo off
+if "%~1"=="" (
+    echo Uso: git-commit.bat "mensagem do commit"
+    exit /b 1
+)
+
+echo Adicionando todos os arquivos...
+git add -A
+
+echo Fazendo commit com a mensagem: %~1
+git commit -m "%~1"
+
+echo Fazendo push...
+git push
+
+echo Concluído!
+pause
